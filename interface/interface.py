174160536e0b71811000b64a1cf687037c8a6aa0
mbari-github <bari.marcello00@gmail.com>
2024-10-03 16:46:52 +0200

    updated interface

--- interface/interface.py
+++ interface/interface.py
@@ -18,37 +18,27 @@ def load_data():
     return pd.DataFrame(records)
 
 # Funzione per aggiornare i dati e visualizzare le metriche
-def update_metrics(veicolo_selezionato):
-    col1, col2 = st.columns(2)
+def update_metrics(veicolo_selezionato):      
+    speed =  veicolo_selezionato['speed'].values[0]
+    acceeleration = veicolo_selezionato['acceleration'].values[0]
+    angle = veicolo_selezionato['angle'].values[0]
+    lane_id = veicolo_selezionato['lane_id'].values[0]
     
-    with col1:
-        st.metric(label="Velocità", value=f"{round(veicolo_selezionato['speed'].values[0], 2)} m/s")
-        st.metric(label="Accelerazione", value=f"{round(veicolo_selezionato['acceleration'].values[0], 2)} m/s²")
-    
-    with col2:
-        st.write("**Angolo:**", round(veicolo_selezionato['angle'].values[0], 2))
-        st.write("**ID corsia:**", veicolo_selezionato['lane_id'].values[0])
+    return speed, acceeleration, angle, lane_id
+
 
 # Funzione per visualizzare le nuove metriche
 def update_additional_metrics(veicolo_selezionato):
-    st.write("### Emissions & Consumptions")
-    row1_col1, row1_col2, row1_col3 = st.columns(3)
-    row2_col1, row2_col2, row2_col3 = st.columns(3)
+    co2_emission = veicolo_selezionato['co2_emission'].values[0]
+    co_emission = veicolo_selezionato['co_emission'].values[0]
+    pm_emission = veicolo_selezionato['pm_emission'].values[0]
+    electricity_consumption = veicolo_selezionato['electricity_consumption'].values[0]
+    fuel_consuption = veicolo_selezionato['fuel_consumption'].values[0]
+    noise = veicolo_selezionato['noise'].values[0]
     
-    with row1_col1:
-        st.write("**CO2 Emission:**", round(veicolo_selezionato['co2_emission'].values[0], 2), "**mg/s**")
-    with row1_col2:
-        st.write("**CO Emission:**", round(veicolo_selezionato['co_emission'].values[0], 2), "**mg/s**")
-    with row1_col3:
-        st.write("**PM Emission:**", round(veicolo_selezionato['pm_emission'].values[0], 2), "**mg/s**")
+    return co2_emission, co_emission, pm_emission, electricity_consumption, fuel_consuption, noise
     
-    with row2_col1:
-        st.write("**Electric Consumption:**", round(veicolo_selezionato['electricity_consumption'].values[0], 2), "**Wh/s**")
-    with row2_col2:
-        st.write("**Fuel Consumption:**", round(veicolo_selezionato['fuel_consumption'].values[0], 2), "**mg/s**")
-    with row2_col3:
-        st.write("**Noise:**", round(veicolo_selezionato['noise'].values[0], 2),"**dB**")
-
+    
 # Funzione per calcolare le metriche aggregate
 def aggregate_metrics(data):
     total_co2 = data['co2_emission'].sum()
@@ -110,10 +100,22 @@ while True:
     veicolo_selezionato = data[data['id'] == st.session_state.last_selected]
 
     if not veicolo_selezionato.empty:
-        st.subheader('Dettagli veicolo selezionato:')
+        st.subheader('Selected vehicle\'s data:')
         
         # Recupera il tipo di veicolo
         vehicle_type = veicolo_selezionato['type'].values[0]
+        
+        
+        #Aggiorna le info
+        speed, acceleration, angle, lane_id = update_metrics(veicolo_selezionato)
+        
+        # Aggiorna le metriche aggiuntive
+        co2_emission, co_emission, pm_emission, electricity_consumption, fuel_consuption, noise = update_additional_metrics(veicolo_selezionato)
+        
+        # Aggiorna le metriche aggregate
+        total_co2, total_co, total_pm, avg_speed, total_fuel_consumption, total_electricity_consumption = aggregate_metrics(data)
+        
+        
         
         # Costruisce il percorso dell'immagine
         image_path = os.path.join(image_folder, f"{vehicle_type}.jpg")
@@ -125,18 +127,46 @@ while True:
             if os.path.exists(image_path):
                 st.image(image_path, caption=vehicle_type)
             else:
-                st.write(f"**Tipo:** {vehicle_type} (Immagine non trovata)")
+                st.write(f"**Model:** {vehicle_type} (Image not found)")
         
         with col_metrics:
-            update_metrics(veicolo_selezionato)
+            col1, col2 = st.columns(2)
+    
+            with col1:
+                st.metric(label="Velocity", value=f"{round(speed, 2)} m/s")
+                st.metric(label="Acceleration", value=f"{round(acceleration , 2)} m/s²")
+            
+            with col2:
+                st.write("**Angle:**", round(angle, 2))
+                st.write("**Road ID:**", lane_id)
+        
+        
+        
         
         # Nuova riga per le nuove metriche
         st.write("---")  # Linea orizzontale per separare le sezioni
-        update_additional_metrics(veicolo_selezionato)
+        st.write("### Emissions & Consumptions")
+        row1_col1, row1_col2, row1_col3 = st.columns(3)
+        row2_col1, row2_col2, row2_col3 = st.columns(3)
+        
+        with row1_col1:
+            st.write("**CO2 Emission:**", round(co2_emission, 2), "**mg/s**")
+        with row1_col2:
+            st.write("**CO Emission:**", round(co_emission, 2), "**mg/s**")
+        with row1_col3:
+            st.write("**PM Emission:**", round(pm_emission, 2), "**mg/s**")
+        
+        with row2_col1:
+            st.write("**Electric Consumption:**", round(electricity_consumption, 2), "**Wh/s**")
+        with row2_col2:
+            st.write("**Fuel Consumption:**", round(fuel_consuption, 2), "**mg/s**")
+        with row2_col3:
+            st.write("**Noise:**", round(noise, 2),"**dB**")
 
-        # Mostra le metriche aggregate
-        total_co2, total_co, total_pm, avg_speed, total_fuel_consumption, total_electricity_consumption = aggregate_metrics(data)
+        
 
+        
+        st.write("---")
         st.write("### Aggregate Metrics")
         col1, col2, col3 = st.columns(3)
 
@@ -153,7 +183,7 @@ while True:
             st.write("**Fuel Consumption:**", round(total_fuel_consumption, 2), "**mg/s**")
         
         with col2:
-            st.write("**Electric Consumption:**", round(total_electricity_consumption, 2), "**mg/s**")
+            st.write("**Electric Consumption:**", round(total_electricity_consumption, 2), "**Wh/s**")
 
         st.write("---")  # Linea orizzontale per separare le sezioni
         st.write("### Average Speed")
