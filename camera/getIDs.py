9953fe7f15ead06bfdb0f6edddfcaa027c09cace
mbari-github <bari.marcello00@gmail.com>
2024-10-21 18:55:19 +0200

    Update getIDs.py

--- camera/getIDs.py
+++ camera/getIDs.py
@@ -4,27 +4,21 @@ def main():
     try:
         # Connettiti al server CARLA
         client = carla.Client('localhost', 2000)
-        client.set_timeout(10.0)  # Timeout di 10 secondi
+        client.set_timeout(10.0)  # Timeout di 10 secondi per evitare che il programma si blocchi
+
+        world = client.get_world()
         print("Connessione al server CARLA avvenuta con successo.")
 
-        # Ottieni il mondo e i veicoli attualmente nella simulazione
-        world = client.get_world()
-
-        # Ottieni tutti gli attori nella simulazione e filtra solo i veicoli
-        vehicles = world.get_actors()
+        vehicles = world.get_actors().filter('vehicle.*')
         IDs = []
-        i=1
-        print(len(vehicles))
+        
         for vehicle in vehicles:
-            i=i+1
             IDs.append(vehicle.id) 
         
-        if IDs:  
+        if vehicles:  
             print("Veicoli trovati:", IDs)
-            print(i)
         else:
             print("Nessun veicolo trovato nella simulazione.")
-            print(i)
 
     except Exception as e:
         print(f"Errore: {e}")
